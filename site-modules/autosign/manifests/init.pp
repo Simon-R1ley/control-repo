5454d93fdfec20d7c52b7958b96688d54daf9d76
Simon Riley <simon.riley@perforce.com>
2023-05-24 16:10:31 +0100

    Autosign Module Add

---
+++ site-modules/autosign/manifests/init.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+# Manage certificate autosigning using the autosign gem
+# A description of what this class does
+# URL: https://forge.puppet.com/modules/danieldreier/autosign/readme
+# @example
+#   include autosign
+class autosign {
+}
