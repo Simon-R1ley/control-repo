24f092a61d34481b4f8b88520480b0ed37efcbce
Simon Riley <simon.riley@perforce.com>
2023-05-30 16:45:40 +0100

    Testing mess

---
+++ site-modules/profile/manifests/autosign.pp
@@ -0,0 +1,24 @@
+#
+class profile::autosign {
+
+  ini_setting { 'policy-based autosigning':
+    setting => 'autosign',
+    path    => "${confdir}/puppet.conf",
+    section => 'master',
+    value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
+    notify  => Service['pe-puppetserver'],
+  }
+
+  class { ::autosign:
+    ensure => 'latest',
+    config => {
+      'general' => {
+        'loglevel' => 'INFO',
+      },
+      'jwt_token' => {
+        'secret'   => 'hunter2',
+        'validity' => '7200',
+      }
+    },
+  }
+}
