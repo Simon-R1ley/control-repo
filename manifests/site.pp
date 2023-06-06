7e3bff1082eb9a23f5cf43a8c651a323b791f69e
Simon Riley <simon.riley@perforce.com>
2023-06-06 19:47:22 +0100

    Basics 2 Testing

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,3 +29,12 @@ node default {
   # Example:
   #   class { 'my_class': }
 }
+
+node 'localhost.localdomain'{
+  ini_setting { 'policy-based autosigning':
+    setting => 'autosign',
+    path    => "${confdir}/puppet.conf",
+    section => 'master',
+    value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
+  }
+}
