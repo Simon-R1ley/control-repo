2dfa35ef4e466c6b262684144d903bff8585faa3
Simon Riley <simon.riley@perforce.com>
2023-07-04 17:06:55 +0100

    Module add

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,10 +1,11 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
-
+  include windows
+  include linux
   # Base profile on fact of node if win or lin
   # https://www.puppet.com/docs/puppet/6/lang_conditional.html
   # include base::windows
   # include base::linux
-
+case $notify['kernel'] ['name']
 
 }
