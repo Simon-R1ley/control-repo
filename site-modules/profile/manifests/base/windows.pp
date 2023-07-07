118fd357b4b62b23c1834d20e326efb5c054f933
Simon Riley <simon.riley@perforce.com>
2023-07-07 11:35:25 +0100

    Group testing

--- site-modules/profile/manifests/base/windows.pp
+++ site-modules/profile/manifests/base/windows.pp
@@ -1,8 +1,12 @@
 #
 class profile::base::windows {
-  # https://www.puppet.com/docs/puppet/6/types/group.html - use for group creation
+  # https://www.puppet.com/docs/puppet/8/types/group.html - use for group creation
   #user { 'Art Vandelay':
 # ensure => present,
   #}
   notify { 'windows': }
+  # Group admin creation
+  group { 'Vandelay Industries Administrators':
+    ensure         => present,
+  }
 }
