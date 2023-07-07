e1aab6cb67c9105a96ff88feaa25526857ecb4b7
Simon Riley <simon.riley@perforce.com>
2023-07-07 12:19:42 +0100

    Case statement change

--- site-modules/role/manifests/server_baseline.pp
+++ site-modules/role/manifests/server_baseline.pp
@@ -1,11 +1,15 @@
 #@This class is used to detect the sever os type, this will be used in conjunction with other classes that are OS specific 
 # Intentional space
 class role::server_baseline {
-  if $facts['os']['family'] == 'windows' {
-    notice('This is a Windows server')
-    include profile::base::windows
-  } else {
-    notice('This is a Linux server')
-    include profile::base::linux
+  case $facts['kernel'] {
+    'windows': { 
+         include profile::base::windows
+    }
+    'Linux':  {
+         include profile::base::linux
+    }
+    default:  {
+         fail('This OS is not supported')
+    }
   }
 }
