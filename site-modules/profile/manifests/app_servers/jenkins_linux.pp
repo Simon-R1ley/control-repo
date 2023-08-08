f881c549a9c5666556afd0edf153e70dfb434ab0
Simon Riley <simon.riley@perforce.com>
2023-08-08 19:39:53 +0100

    rem space

--- site-modules/profile/manifests/app_servers/jenkins_linux.pp
+++ site-modules/profile/manifests/app_servers/jenkins_linux.pp
@@ -34,7 +34,7 @@ class profile::app_servers::jenkins_linux {
   service { 'jenkins':
     ensure  => 'running',
     enable  => 'true',
-    start   => ' --httpPort=8000',
+    start   => '--httpPort=8000',
     require => [Package['jenkins']],
   }
 
