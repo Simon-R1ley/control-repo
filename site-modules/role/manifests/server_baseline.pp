1ee44b20d1bdd4a4db3b0ff2bd9c5db58f9432e8
Simon Riley <simon.riley@perforce.com>
2023-09-04 14:46:40 +0100

    8080

--- site-modules/role/manifests/server_baseline.pp
+++ site-modules/role/manifests/server_baseline.pp
@@ -1,5 +1,6 @@
 # This class is used to detect the sever os type, this will be used in conjunction with other classes that are OS specific 
 # Intentional space
+# review roles and profiles
 class role::server_baseline {
   case $facts['kernel'] {
     'windows': {
