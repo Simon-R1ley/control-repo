eaf786e95e715fe9f1a7ee000a47a234d5a47abe
Simon Riley <simon.riley@perforce.com>
2023-09-08 14:24:22 +0100

    change

--- site-modules/role/manifests/jenkins_server.pp
+++ site-modules/role/manifests/jenkins_server.pp
@@ -1,7 +1,7 @@
 # This class is used to detect the sever os type, this will be used in conjunction with other classes that are OS specific 
 # Intentional space
 # review roles and profiles
-class role::server_baseline {
+class role::jenkins_server {
   include profile::base
   include profile::jenkins
 }
