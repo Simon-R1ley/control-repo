91e7cca4f8e86d0ffa507810f591677f23d7a0ef
Simon Riley <simon.riley@perforce.com>
2024-03-01 21:43:59 +0000

    Script refactor

--- site-modules/autopatch/tasks/checkingroot.sh
+++ site-modules/autopatch/tasks/checkingroot.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-rootspace=`df / | awk '/[0-9]%/{print $(NF-2)}'`
+rootspace=$(df / | awk '/[0-9]%/{print $(NF-2)}')
 
-if [ $rootspace -gt '10000' ]; then
+if [ "$rootspace" -gt 10000 ]; then
   result=0
 else
   result=1
 fi
 
-exit $result
+exit "$result"
