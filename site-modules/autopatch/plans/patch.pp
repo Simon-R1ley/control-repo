55f592766c72cb90e59ac808624a5a5426ce24a7
Simon Riley <simon.riley@perforce.com>
2024-03-03 00:22:21 +0000

    updated script

--- site-modules/autopatch/plans/patch.pp
+++ site-modules/autopatch/plans/patch.pp
@@ -2,12 +2,20 @@
 plan autopatch::patch (
   TargetSpec $targets,
 ) {
+  # Invoke the task and store the result in $rootstatus
   $rootstatus = run_task('autopatch::checkingroot', $targets)
 
-  if $result != Integer('0') {
-    fail_plan('Root partition is full')
+  # Access the exit_code attribute to obtain the exit code
+  $exit_code = $rootstatus['exit_code']
+
+  # Check the exit code and take appropriate actions
+  if $exit_code == 0 {
+    notify { 'Root space check succeeded': }
+  } else {
+    notify { 'Root space check failed': }
   }
 
+  # Continue with patching logic
   $patchstatus = run_task('autopatch::patch', $targets)
 
   if $patchstatus != 0 {
